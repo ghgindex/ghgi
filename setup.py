53a9cd5c02fba6402326ede52ecef2316c280595
Geoff Pitfield <geoff@pitfield.com>
2020-12-27 14:31:41 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ghgi',
+      version='0.0.1',
+      description='Greenhouse Gas Index',
+      url='https://github.com/ghgindex/ghgi',
+      author='GHGI',
+      author_email='geoff@ghgi.org',
+      license='CCv4-BY-SA',
+      packages=['ghgi'],
+      zip_safe=False)
