569a850410eccf704e613aa97ad5674c67c908d0
Geoff Pitfield <gpitfield@Geoffs-MacBook-Air.local>
2021-10-06 12:36:37 -0700

    regenerate index

--- ghgi/tests/fixtures/gin.py
+++ ghgi/tests/fixtures/gin.py
@@ -3,5 +3,9 @@
 
 QUERIES = [
     ('dried wide wheat noodle',
-     ('wheat noodle', ['noodle', 10], 0.6666666666666666))
+     ('wheat noodle', ['noodle', 10], 0.6666666666666666)),
+    ('parmigiano-reggiano',
+     ('parmigiano-reggiano', ['cheese', 17], 1.0)),
+    ('parmigiano reggiano',
+     ('parmigiano reggiano', ['cheese', 17], 1.0))
 ]
